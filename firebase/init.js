22854a46b2fbe9528fede3e43b991aeafc97e960
hiromitsu-yokoyama <hyokoyama@i.gmobb.jp>
2023-03-02 19:44:06 +0900

    test

---
+++ firebase/init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoDx8rLwowfAnY8OFC9Fc9yUlk7lA1ewM",
+  authDomain: "todo-next-firebase-28906.firebaseapp.com",
+  projectId: "todo-next-firebase-28906",
+  storageBucket: "todo-next-firebase-28906.appspot.com",
+  messagingSenderId: "704879920179",
+  appId: "1:704879920179:web:fe92f32fddd091a7df0f46"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
